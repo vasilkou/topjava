cb4e87bc6a459b349d045f7c45768b39c798628f
vasilkou <yury.vasilkou@gmail.com>
2017-01-30 16:25:48 +0300

    My-HW7-Added_RootControllerTest.testMeals_and_ResourceControllerTest

--- src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
@@ -6,6 +6,7 @@ import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static ru.javawebinar.topjava.MealTestData.*;
 import static ru.javawebinar.topjava.UserTestData.USER;
 import static ru.javawebinar.topjava.model.BaseEntity.START_SEQ;
 
@@ -28,6 +29,37 @@ public class RootControllerTest extends AbstractControllerTest {
                                 hasProperty("id", is(START_SEQ)),
                                 hasProperty("name", is(USER.getName()))
                         )
+                )));
+    }
+
+    @Test
+    public void testMeals() throws Exception {
+        mockMvc.perform(get("/meals"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("meals"))
+                .andExpect(forwardedUrl("/WEB-INF/jsp/meals.jsp"))
+                .andExpect(model().attribute("meals", hasSize(6)))
+                .andExpect(model().attribute("meals", hasItem(
+                        allOf(
+                                hasProperty("id", is(MEAL1_ID)),
+                                hasProperty("dateTime", is(MEAL1.getDateTime())),
+                                hasProperty("description", is(MEAL1.getDescription())),
+                                hasProperty("calories", is(MEAL1.getCalories())),
+                                hasProperty("exceed", is(false))
+                        )
+                )))
+                .andExpect(model().attribute("meals", hasItem(
+                        allOf(
+                                hasProperty("id", is(MEAL1_ID + 4)),
+                                hasProperty("dateTime", is(MEAL5.getDateTime())),
+                                hasProperty("description", is(MEAL5.getDescription())),
+                                hasProperty("calories", is(MEAL5.getCalories())),
+                                hasProperty("exceed", is(true))
+                        )
+                )))
+                .andExpect(model().attribute("meals", not(
+                        hasItem(hasProperty("id", is(ADMIN_MEAL_ID)))
                 )));
     }
 }
